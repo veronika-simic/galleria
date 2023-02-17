b78bd2594cdabe3d1e6885cd2f078335218f1668
veronika-simic <veronikasimic21@gmail.com>
2023-02-17 10:57:13 +0100

    create masonry grid

--- src/components/HomePage/Home.tsx
+++ src/components/HomePage/Home.tsx
@@ -1,19 +1,25 @@
+import Box from '@mui/material/Box';
+import Masonry from '@mui/lab/Masonry';
 import { useState, useEffect } from 'react';
 import { getData } from '../api/axios';
 export default function HomePage() {
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<any>([]);
 
   useEffect(() => {
-    const getAllDataFromApi = async() => {
-        const response = await getData();
-        setData(response);
-    }
-    getAllDataFromApi()
-  }, [setData]);
+    const getAllDataFromApi = async () => {
+      const response = await getData();
+      setData(response);
+    };
+    getAllDataFromApi();
+  }, []);
 
   return (
-    <>
-      <p>{data}</p>
-    </>
+    <Box>
+      <Masonry columns={4} spacing={2}>
+        {data.map((data: string) => (
+          <img key={data} src={data}/>
+        ))}
+      </Masonry>
+    </Box>
   );
 }
