49b876120f3fc44744d2fec0132f00b831bc442a
veronika-simic <veronikasimic21@gmail.com>
2023-02-17 10:25:27 +0100

    display image url on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import HomePage from './components/HomePage/Home';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/components/HomePage/Home.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import { getData } from '../api/axios';
+export default function HomePage() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getAllDataFromApi = async() => {
+        const response = await getData();
+        setData(response);
+    }
+    getAllDataFromApi()
+  }, [setData]);
+
+  return (
+    <>
+      <p>{data}</p>
+    </>
+  );
+}
