ee9bb1b004a2e0e9e6540d8baf8433b1e2dcfb6b
veronika-simic <veronikasimic21@gmail.com>
2023-02-22 15:48:35 +0100

    add caption on images

--- src/components/SingleImage/Image.tsx
+++ src/components/SingleImage/Image.tsx
@@ -1,22 +1,34 @@
 import { ArtifactModel } from '../../models/Artifacts';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 export default function SingleImage(props: { data: ArtifactModel }) {
   return (
     <Card>
-      <CardMedia
-        component="img"
-        image={props.data.webImage.url}
-        alt={props.data.longTitle}
-      />
-      <Typography variant="h5" color={'whitesmoke'}>
-        {props.data.title}
-      </Typography>
-      <Typography variant="h6" color={'whitesmoke'}>
-        {props.data.principalOrFirstMaker}
-      </Typography>
+      <Box sx={{ position: 'relative' }}>
+        <CardMedia
+          component="img"
+          image={props.data.webImage.url}
+          alt={props.data.longTitle}
+        />
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: '15%',
+            left: '15%',
+            width: '100%',
+            color: 'white',
+          }}
+        >
+          <Typography variant="h5" color={'whitesmoke'}>
+            {props.data.title}
+          </Typography>
+          <Typography variant="h6" color={'whitesmoke'}>
+            {props.data.principalOrFirstMaker}
+          </Typography>
+        </Box>
+      </Box>
     </Card>
   );
 }
