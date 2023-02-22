5cf1920b01282f35af7503f8949c438bb67728bf
veronika-simic <veronikasimic21@gmail.com>
2023-02-22 07:20:11 +0100

    flexbox for logo navbar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,11 +1,20 @@
 import { Divider, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
+
 export default function NavigationBar() {
   return (
-    <Box>
-      <Typography variant="h2">galleria.</Typography>
-      <Typography>START SLIDESHOW</Typography>
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h2">galleria.</Typography>
+        <Typography>START SLIDESHOW</Typography>
+      </Box>
       <Divider />
-    </Box>
+    </>
   );
 }
