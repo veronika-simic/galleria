181fa5406cf7109eb53602a84c01e3dea8f07d4e
veronika-simic <veronikasimic21@gmail.com>
2023-02-22 15:36:05 +0100

    render title and author of each artifact

--- src/components/SingleImage/Image.tsx
+++ src/components/SingleImage/Image.tsx
@@ -1,5 +1,22 @@
 import { ArtifactModel } from '../../models/Artifacts';
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 export default function SingleImage(props: { data: ArtifactModel }) {
-  return <img src={props.data.webImage.url} alt={props.data.longTitle} loading="lazy" />;
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        image={props.data.webImage.url}
+        alt={props.data.longTitle}
+      />
+      <Typography variant="h5" color={'whitesmoke'}>
+        {props.data.title}
+      </Typography>
+      <Typography variant="h6" color={'whitesmoke'}>
+        {props.data.principalOrFirstMaker}
+      </Typography>
+    </Card>
+  );
 }
--- src/components/api/axios.ts
+++ src/components/api/axios.ts
@@ -16,7 +16,7 @@ export async function getDataById(id:number) {
 export async function getData() {
   try {
     const response = await axios.get(
-      'https://www.rijksmuseum.nl/api/en/collection?key=4qJ0emUy&ps=100'
+      'https://www.rijksmuseum.nl/api/en/collection?key=4qJ0emUy&ps=50'
     );
     const allData = [];
     for (let i = 0; i < response.data.artObjects.length; i += 1) {
