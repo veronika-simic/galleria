6bd0684785abef64406045343d7555226035a315
veronika-simic <veronikasimic21@gmail.com>
2023-02-22 08:50:57 +0100

    remove horizontal overflow

--- src/components/HomePage/Home.tsx
+++ src/components/HomePage/Home.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
   }, []);
 
   return (
-    <Box>
+    <Box sx={{ overflowY: 'hidden' }}>
       <Masonry columns={{ xs: 1, sm: 2, md: 2, lg: 4 }} spacing={{ xs: 2 }}>
         {loading ? (
           <Skeleton />
