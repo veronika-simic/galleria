7fa2a75719afc2516b1e705becb9e62c5ca7c4b9
veronika-simic <veronikasimic21@gmail.com>
2023-02-25 15:15:19 +0100

    routing for main page and 404

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material';
+
+export default function ErrorPage() {
+  return (
+    <Box sx={{}}>
+      <Typography>OOOOOPS !</Typography>
+      <Typography>
+        Sorry for the inconvenience. The responsible person will be fired.
+        (thanks to you)
+      </Typography>
+    </Box>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import HomePage from './components/HomePage/Home';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
