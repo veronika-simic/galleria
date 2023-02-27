94bace245009aa958c71fd4ec96f13e91b94eaac
veronika-simic <veronikasimic21@gmail.com>
2023-02-27 09:00:45 +0100

    render data from axios call

--- src/components/InnerPage/InnerPage.tsx
+++ src/components/InnerPage/InnerPage.tsx
@@ -1,21 +1,19 @@
 import { getDataById } from '../api/axios';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { ArtifactData } from '../../models/ArtifactData';
 
 export default function InnerPage() {
   const { artifactId } = useParams();
-  const [artifactData, setArtifactData] = useState<any>();
+  const [artifactData, setArtifactData] = useState<any>([]);
   useEffect(() => {
     const getArtifactData = async () => {
       const response = await getDataById(artifactId);
       setArtifactData(response);
     };
     getArtifactData();
-  },[]);
-  return (
-    <>
-      <h2>{artifactId}</h2>
-      <h2>{artifactData.elapsedMilliseconds}</h2>
-    </>
-  );
+  }, []);
+  return <>
+    <h1>{artifactData.id}</h1>
+  </>;
 }
