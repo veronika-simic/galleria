73e134196bd5c61a63bbc9dd9fa9da7963bc54b9
veronika-simic <veronikasimic21@gmail.com>
2023-02-28 07:35:39 +0100

    position elements correctly

--- src/components/InnerPage/InnerPage.tsx
+++ src/components/InnerPage/InnerPage.tsx
@@ -1,8 +1,8 @@
 import { getDataById } from '../api/axios';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { ArtifactData } from '../../models/ArtifactData';
-import CardMedia from '@mui/material/CardMedia';
+import Box from '@mui/material/Box';
+import { Card, CardMedia, Typography } from '@mui/material';
 
 export default function InnerPage() {
   const { artifactId } = useParams();
@@ -15,11 +15,26 @@ export default function InnerPage() {
     getArtifactData();
   }, []);
   return (
-    <>
-      <h1>{artifactData.title}</h1>
-      <h1>{artifactData.principalMaker}</h1>
-      <img src={artifactData.webImage.url} />
-    
-    </>
+    <Box
+      sx={{ display: 'flex', justifyContent: 'space-between', width: '95%' }}
+    >
+      <Box sx={{ width: '50%', border: '1px solid red', display: 'flex' }}>
+        <Card
+          sx={{
+            maxWidth: '70%',
+          }}
+        >
+          <CardMedia src={artifactData?.webImage?.url} component="img" />
+        </Card>
+        <Box>
+          <Typography>{artifactData.title}</Typography>
+          <Typography>{artifactData.principalMaker}</Typography>
+        </Box>
+      </Box>
+
+      <Box sx={{ width: '50%', border: '1px solid red' }}>
+        <h1>description</h1>
+      </Box>
+    </Box>
   );
 }
