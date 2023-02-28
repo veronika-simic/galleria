533932343e0ccc83f42cedd93d310a9cd66ff458
veronika-simic <veronikasimic21@gmail.com>
2023-02-28 08:27:57 +0100

    format the year

--- src/components/InnerPage/InnerPage.tsx
+++ src/components/InnerPage/InnerPage.tsx
@@ -28,6 +28,7 @@ export default function InnerPage() {
           display: 'flex',
           position: 'relative',
           width: '50%',
+          border: '1px solid red',
         }}
       >
         <Card
@@ -61,9 +62,29 @@ export default function InnerPage() {
         </Box>
       </Box>
 
-      <Box sx={{ width: '50%', textAlign: 'center' }}>
-        <h1>{artifactData.dating?.sortingDate}</h1>
-        <h1>description</h1>
+      <Box sx={{ width: '50%', textAlign: 'center', border: '1px solid red', position: 'relative' }}>
+        <Typography
+          variant="subtitle2"
+          fontSize={'14rem'}
+          fontFamily={'Noto Serif'}
+          color={'#EEEEEE'}
+          zIndex={-10}
+          position="absolute"
+          right={'5rem'}
+        >
+          {artifactData.dating?.sortingDate}
+        </Typography>
+        <Typography>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem.
+        </Typography>
       </Box>
     </Box>
   );
