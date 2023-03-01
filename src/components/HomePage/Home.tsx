bb24a5136a5ddd4b090b5df35bf16a95cef9feed
veronika-simic <veronikasimic21@gmail.com>
2023-03-01 07:29:21 +0100

    add skeleton loader

--- src/components/HomePage/Home.tsx
+++ src/components/HomePage/Home.tsx
@@ -22,16 +22,16 @@ export default function HomePage() {
       sx={{
         margin: '2rem auto 0',
         width: '95%',
-        overflow: 'hidden'
+        overflow: 'hidden',
       }}
     >
       <Masonry
         columns={{ xs: 1, sm: 2, md: 2, lg: 4 }}
-        spacing={{ xs: 2}}
+        spacing={{ xs: 2 }}
         sx={{ height: 'auto', width: 'auto' }}
       >
         {data.map((data: ArtifactModel) => (
-          <Artifact key={data.objectNumber} data={data} />
+          <Artifact key={data.objectNumber} data={data} loading={loading} />
         ))}
       </Masonry>
     </Box>
--- src/components/SingleImage/Artifact.tsx
+++ src/components/SingleImage/Artifact.tsx
@@ -5,34 +5,46 @@ import { useState } from 'react';
 import ImageFile from './ImageFile';
 import ImageCaption from './ImageCaption';
 import { Link } from 'react-router-dom';
-export default function Artifact(props: { data: ArtifactModel }) {
+import { Skeleton } from '@mui/material';
+export default function Artifact(props: {
+  data: ArtifactModel;
+  loading: boolean;
+}) {
   const [onHover, setOnHover] = useState({ raised: false, shadow: 1 });
-
+  console.log(props.loading);
   return (
-    <Link to={`artifacts/` + props.data.objectNumber}>
-    <Card
-      onMouseOver={() => setOnHover({ raised: true, shadow: 10 })}
-      onMouseLeave={() => setOnHover({ raised: false, shadow: 1 })}
-      raised={onHover.raised}
-    >
-      <Box sx={{ position: 'relative' }}>
-        <ImageFile data={props.data} />
-        <Box
-          sx={{
-            position: 'absolute',
-            bottom: '5%',
-            left: '5%',
-            width: '80%',
-            color: 'whitesmoke',
-          }}
-        >
-          <ImageCaption
-            title={props.data.title}
-            author={props.data.principalOrFirstMaker}
-          />
-        </Box>
-      </Box>
-    </Card>
-    </Link>
+    <>
+      {props.loading ? (
+        <Skeleton variant="rectangular" color="grey">
+          <ImageFile data={props.data} />
+        </Skeleton>
+      ) : (
+        <Link to={`artifacts/` + props.data.objectNumber}>
+          <Card
+            onMouseOver={() => setOnHover({ raised: true, shadow: 10 })}
+            onMouseLeave={() => setOnHover({ raised: false, shadow: 1 })}
+            raised={onHover.raised}
+          >
+            <Box sx={{ position: 'relative' }}>
+              <ImageFile data={props.data} />
+              <Box
+                sx={{
+                  position: 'absolute',
+                  bottom: '5%',
+                  left: '5%',
+                  width: '80%',
+                  color: 'whitesmoke',
+                }}
+              >
+                <ImageCaption
+                  title={props.data.title}
+                  author={props.data.principalOrFirstMaker}
+                />
+              </Box>
+            </Box>
+          </Card>
+        </Link>
+      )}
+    </>
   );
 }
